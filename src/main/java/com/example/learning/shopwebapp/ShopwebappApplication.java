e8eddfa94ba3041453e7fa20b154f087e91e0180
MHD Husam Takaji <mhdhusamtakaji@gmail.com>
2024-05-09 11:33:41 +0400

    setting up project

---
+++ src/main/java/com/example/learning/shopwebapp/ShopwebappApplication.java
@@ -0,0 +1,13 @@
+package com.example.learning.shopwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopwebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/learning/shopwebapp/ShopwebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learning.shopwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopwebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
